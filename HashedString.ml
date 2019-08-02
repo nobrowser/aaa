fe2c1dfa861369b32b4fa1463261d849a3f7bb0b
Ian Zimmerman <itz@no-use.mooo.com>
2019-08-02 09:55:20 -0700

    Improve formatting

--- HashedString.ml
+++ HashedString.ml
@@ -1,6 +1,6 @@
 include String
 
 let hash s =
-  let d = Digest.(string s |> to_hex) in
-  let l = if Sys.int_size <= 32 then 7 else 15 in
-  int_of_string ("0x" ^ String.sub d 13 l)
+    let d = Digest.(string s |> to_hex) in
+    let l = if Sys.int_size <= 32 then 7 else 15 in
+    int_of_string ("0x" ^ String.sub d 13 l)
