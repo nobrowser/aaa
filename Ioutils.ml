480dea6390950f99baf92cde4f77a681ed335821
Ian Zimmerman <itz@no-use.mooo.com>
2019-08-09 09:39:37 -0700

    Add general format-directed iteration on channels

--- Ioutils.ml
+++ Ioutils.ml
@@ -1,19 +1,38 @@
 let with_file fn ~f:reader =
-  let ch = open_in fn in
-  Exnutils.protect ~finally:(fun () -> close_in ch) ~f:(fun () -> reader ch)
+    let ch = open_in fn in
+    Exnutils.protect ~finally:(fun () -> close_in ch) ~f:(fun () -> reader ch)
 
-let seq_of_in_chan inch =
-  let rec delay () =
-    try Seq.Cons (input_line inch, delay) with
-    | End_of_file -> Seq.Nil
-  in delay
+let lines_of_in_chan inch =
+    let rec delay () =
+      Seq.(try Cons (input_line inch, delay) with End_of_file -> Nil)
+    in delay
 
 let fold_lines ~f ~init inch =
-  Seq.fold_left f init (seq_of_in_chan inch)
+    Seq.fold_left f init (lines_of_in_chan inch)
 
 let iter_lines ~f inch =
-  Seq.iter f (seq_of_in_chan inch)
+    Seq.iter f (lines_of_in_chan inch)
 
 let fold_file_lines ~f:lf ~init fn = with_file fn ~f:(fold_lines ~f:lf ~init)
 
 let iter_file_lines ~f:lf fn = with_file fn ~f:(iter_lines ~f:lf)
+
+let chars_of_in_chan inch =
+    let rec delay () =
+      Seq.(try Cons (input_char inch, delay) with End_of_file -> Nil)
+    in delay
+
+let fold_chars ~f ~init inch =
+    Seq.fold_left f init (chars_of_in_chan inch)
+
+let iter_chars ~f inch =
+    Seq.iter f (chars_of_in_chan inch)
+
+let fold_file_chars ~f:lf ~init fn = with_file fn ~f:(fold_chars ~f:lf ~init)
+
+let iter_file_chars ~f:lf fn = with_file fn ~f:(iter_chars ~f:lf)
+
+let seq_of_in_chan inch ~fmt ~f =
+    let reader = Scanf.(bscanf (Scanning.from_channel inch) fmt) in
+    let rec delay () = Seq.(try Cons (reader f, delay) with End_of_file -> Nil)
+    in delay
--- Ioutils.mli
+++ Ioutils.mli
@@ -1,6 +1,6 @@
 val with_file : string -> f:(in_channel -> 'a) -> 'a
 
-val seq_of_in_chan : in_channel -> string Seq.t
+val lines_of_in_chan : in_channel -> string Seq.t
 
 val fold_lines : f:('a -> string -> 'a) -> init:'a -> in_channel -> 'a
 
@@ -9,3 +9,18 @@ val iter_lines : f:(string -> unit) -> in_channel -> unit
 val fold_file_lines : f:('a -> string -> 'a) -> init:'a -> string -> 'a
 
 val iter_file_lines : f:(string -> unit) -> string -> unit
+
+val chars_of_in_chan : in_channel -> char Seq.t
+
+val fold_chars : f:('a -> char -> 'a) -> init:'a -> in_channel -> 'a
+
+val iter_chars : f:(char -> unit) -> in_channel -> unit
+
+val fold_file_chars : f:('a -> char -> 'a) -> init:'a -> string -> 'a
+
+val iter_file_chars : f:(char -> unit) -> string -> unit
+
+val seq_of_in_chan : in_channel ->
+    fmt:(('a, Scanf.Scanning.in_channel, 'b, 'c -> 'd, 'a -> 'e, 'e) format6) ->
+    f:'c ->
+    'd Seq.t
