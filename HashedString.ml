1b5848453ba378db587a8d60ee486b990cd415d1
Ian Zimmerman <itz@no-use.mooo.com>
2019-08-30 05:36:48 -0700

    Simplify hash function

--- HashedString.ml
+++ HashedString.ml
@@ -1,6 +1,8 @@
 include String
 
 let hash s =
-    let d = Digest.(string s |> to_hex) in
-    let l = if Sys.int_size <= 32 then 7 else 15 in
-    int_of_string ("0x" ^ String.sub d 13 l)
+    let d = Digest.string s in
+    Char.(((code d.[3] land 0x3f) lsl 24)
+          lor (code d.[7] lsl 16)
+          lor (code d.[11] lsl 8)
+          lor code d.[13])
