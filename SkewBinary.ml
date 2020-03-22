6f2a3f001eb521e7905e62758be4c6077acd62f9
Ian Zimmerman <itz@no-use.mooo.com>
2020-03-21 23:58:17 -0700

    Add module for Skew Binary numbers

---
+++ SkewBinary.ml
@@ -0,0 +1,35 @@
+(** type of skew binary integers: list of nonzero weights in increasing order *)
+type t = int list
+
+(** Get the integer value of a skew binary number *)
+let to_int s = List.fold_left (+) 0 s
+
+(** Increment a skew binary number *)
+let inc = function
+  | w1 :: w2 :: ws when w1 = w2 -> (w1 + w2 + 1) :: ws
+  | ws -> 1 :: ws
+
+(** Decrement a skew binary number *)
+let dec = function
+  | [] -> invalid_arg "SkewBinary.dec"
+  | 1 :: ws -> ws
+  | w :: ws -> let w' = w / 2 in w' :: w' :: ws
+
+let rec inc' i ws = match i with 0 -> ws | i -> inc' (i - 1) (inc ws)
+
+let bit_positions i =
+  let rec bit_positions' a p = function
+  | 0 -> a
+  | i ->
+     let a1 = if i mod 2 = 0 then a else p :: a in
+     bit_positions' a1 (p + 1) (i lsr 1)
+  in List.rev (bit_positions' [] 0 i)
+
+(** Construct the skew binary number corresponding to an integer *)
+let of_int i =
+  let bp = bit_positions i in
+  let l = List.length bp in
+  let bp' = match bp with
+  | 0 :: rest -> rest
+  | _ -> bp in
+  bp' |> List.map (fun i -> (1 lsl i) - 1) |> inc' l
---
+++ SkewBinary.mli
@@ -0,0 +1,16 @@
+(** Skew binary numbers as per Okasaki *)
+
+(** Type of skew binary numbers *)
+type t
+
+(** Get the integer value of a skew binary number *)
+val to_int : t -> int
+
+(** Construct the skew binary number corresponding to an integer *)
+val of_int : int -> t
+
+(** Increment a skew binary number *)
+val inc : t -> t
+
+(** Decrement a skew binary number *)
+val dec : t -> t
